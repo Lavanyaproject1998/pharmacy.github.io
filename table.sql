c94065787aa496a90b04f9d643114f9121f0f87c
Lavanyaproject1998 <Likhithamgowda.cv@gmail.com>
2025-04-15 20:17:20 +0530

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,15 @@
+ create database mydb;
+
+ use mydb;
+
+create table user_info
+(
+portalid int primary key, 
+name varchar(20), 
+employeeId int, 
+technology varchar(20), 
+password varchar(20)
+);
+
+
+ -- create c:\logs folder at c: drive
